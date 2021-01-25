01fb9d114ce49d11615e6bb3ba1d145b91ae5fa6
bj75326 <superice_gy@163.com>
2021-01-25 17:00:23 +0800

    chore: test eslint

--- packages/pk02/src/index.tsx
+++ packages/pk02/src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Button } from 'antd';
 
-export interface HelloWorldProps {}
+export interface HelloWorldProps {} // eslint-disable-line
 
-const HelloWorld: React.FC<HelloWorldProps> = (props) => (
+const HelloWorld: React.FC<HelloWorldProps> = () => (
   <Button type="primary">Hello PK02 --watch</Button>
 );
 
